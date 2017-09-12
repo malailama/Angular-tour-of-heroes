db1623175fd7765ce3ac4f101fd655e9e03b4969
Akshay Rana <akshay1301@gmail.com>
2017-09-12 19:55:53 +0530

    First stable version of Rounting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,25 @@ import { FormsModule}    from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule 
+  ],
+  declarations: [ 
+    AppComponent, 
+    HeroDetailComponent, 
+    HeroesComponent 
+  ],
+  providers: [ 
+    HeroService 
+  ],
+  bootstrap:  [ 
+    AppComponent 
+  ]
 })
+
 export class AppModule { }
