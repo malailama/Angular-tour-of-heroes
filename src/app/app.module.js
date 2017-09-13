345abc206ee46f87b3d9427e1a15976801dc65fd
Akshay Rana <akshay1301@gmail.com>
2017-09-13 10:49:36 +0530

    Dashboard, Navigation and back button added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -9,9 +9,14 @@ var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
+var router_1 = require("@angular/router");
 var hero_detail_component_1 = require("./hero-detail.component");
 var hero_service_1 = require("./hero.service");
 var heroes_component_1 = require("./heroes.component");
+var dashboard_component_1 = require("./dashboard.component");
+//#4 If Router Module is placed under @NgModule then an error is being thrown. Check out why.
+//The Router module has been directlt palced in the imports section of @NgModule
+//The error was their because component class must be followed by @ decorator
 var AppModule = (function () {
     function AppModule() {
     }
@@ -21,10 +26,30 @@ AppModule = __decorate([
     core_1.NgModule({
         imports: [
             platform_browser_1.BrowserModule,
-            forms_1.FormsModule
+            forms_1.FormsModule,
+            router_1.RouterModule.forRoot([
+                {
+                    path: '',
+                    redirectTo: '/dashboard',
+                    pathMatch: 'full'
+                },
+                {
+                    path: 'detail/:id',
+                    component: hero_detail_component_1.HeroDetailComponent
+                },
+                {
+                    path: "heroes",
+                    component: heroes_component_1.HeroesComponent
+                },
+                {
+                    path: 'dashboard',
+                    component: dashboard_component_1.DashboardComponent
+                }
+            ])
         ],
         declarations: [
             app_component_1.AppComponent,
+            dashboard_component_1.DashboardComponent,
             hero_detail_component_1.HeroDetailComponent,
             heroes_component_1.HeroesComponent
         ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule}    from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { RouterModule } from '@angular/router';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+//#4 If Router Module is placed under @NgModule then an error is being thrown. Check out why.
+//The Router module has been directlt palced in the imports section of @NgModule
+//The error was their because component class must be followed by @ decorator
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
-    FormsModule 
+    FormsModule,
+    RouterModule.forRoot ([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+      {
+        path: `heroes`,
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]) 
   ],
   declarations: [ 
     AppComponent, 
+    DashboardComponent,
     HeroDetailComponent, 
     HeroesComponent 
   ],
@@ -26,3 +52,5 @@ import { HeroesComponent } from './heroes.component';
 })
 
 export class AppModule { }
+
+
